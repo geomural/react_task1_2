1c043910b17cd8192d9083db6b7044d1e50be384
Alexandra <Alexandra@DESKTOP-LES4KNQ>
2020-09-03 20:58:28 +0300

    task1.2

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import './css/main.css';
+import ShopItemClass from './components/ShopItemClass';
+import ShopItemModel from './models/ShopItemModel';
+
+function App() {
+  const item = new ShopItemModel(
+    'Tiger of Sweden',
+    'Leonard coat',
+    'Minimalistic coat in cotton-blend',
+    'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    399,
+    '£'
+  );
+
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
